235b9a88cba4e6523073c417ff910c0a3c226d2c
umaima-alrawahi <omaima97alrawahi@gmail.com>
2022-11-17 00:30:25 +0400

    add Photo component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Photo from './components/Photo.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+ 
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Suspense>
+    <Photo class="async-component" />
+    <template #fallback>
+      <SimpleLoading class="async-component" />
+    </template>
+  </Suspense>
+  
 </template>
 
 <style scoped>
---
+++ src/components/Photo.vue
@@ -0,0 +1,32 @@
+<script setup>
+	import { ref, onErrorCaptured } from 'vue'
+	const response = ref({})
+
+  // adding delay
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, 1000)
+  })
+
+  onErrorCaptured((err) => {
+    // error processing
+  })
+
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos/' + Math.floor(Math.random() * 10) + 1);
+  console.log(res)
+  if (!res.ok) {
+    throw new Error()
+  }
+  response.value = (await res.json())
+
+</script>
+
+<template>
+  <div>
+    <p>{{ response.albumId }}</p>
+    <p>{{ response.title }}</p>
+    <img :src="response.thumbnailUrl" />
+  </div>
+</template>
