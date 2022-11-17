d37a9402bb43e272e6208b2a1ec37a8cae7753f7
umaima-alrawahi <omaima97alrawahi@gmail.com>
2022-11-17 15:12:35 +0400

    router

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@ import Photo from './components/Photo.vue'
 </script>
 
 <template>
+  <header>
+    <RouterLink to="/" >Home</RouterLink> <br />
+    <RouterLink :to="{name : 'about'}" >about</RouterLink>
+  </header>
   <div>
- 
+ <RouterView></RouterView>
   </div>
   <Suspense>
     <Photo class="async-component" />
